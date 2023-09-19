badaf2136fa7cbb656605a1587f0b265c74b9a4e
Nikita G <jobnikitka152rus@gmail.com>
2023-09-19 17:59:23 +0300

    homeworks

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let result;
+  let d = b**2 - 4 * a * c;
+  if (d < 0) {
+    return arr;
+  }
+  else if (d === 0) {
+   result = -b / (2 * a);
+   arr.push(result);
+ return arr;
+  }
+  else if (d > 0) {
+   result = (-b + Math.sqrt(d)) / (2 * a);
+   arr.push(result);
+   result = (-b - Math.sqrt(d)) / (2 * a);
+   arr.push(result);
+  };
   return arr;
-}
+};
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+  percent = (percent / 100) / 12;
+  let sum = amount - contribution;
+  let monthlyFee = sum * (percent + (percent / (((1 + percent)**countMonths) - 1)));
+  let total = monthlyFee*countMonths;
+  return Number(total.toFixed(2));
+};
