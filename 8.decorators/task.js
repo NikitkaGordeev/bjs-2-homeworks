68bfb77a182effa2e44de79e7a7748db23ed380f
Nikita G <jobnikitka152rus@gmail.com>
2023-10-19 18:06:07 +0300

    add task

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,40 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+    function wrapper(...args) {
+        const hash = md5(args);
+        let objectInCache = cache.find((item) => item.hash === hash);
+        if (objectInCache) {
+            console.log("Из кеша: " + objectInCache.element);
+            return "Из кеша: " + objectInCache.element;
+        }
+        let result = func(...args);
+        cache.push({ hash, element: result });
+        if (cache.length > 5) {
+            cache.shift();
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+    return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timerId = null;
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    function wrapper(...args) {
+        if (!timerId){
+            func(...args);
+            wrapper.count++;
+        }
+        wrapper.allCount++;
+        clearTimeout(timerId);
+        timerId = setTimeout(() => {
+            func(...args);
+            wrapper.count++;
+        }, delay)
+    }
+    return wrapper;
 }
